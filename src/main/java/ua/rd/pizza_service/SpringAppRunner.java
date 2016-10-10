56e4896171766187f25455825ceb6d811be19b2e
Artem Trybel <Artem_Trybel@epam.com>
2016-10-10 14:02:45 +0300

    Create dao and service context

---
+++ src/main/java/ua/rd/pizza_service/SpringAppRunner.java
@@ -0,0 +1,37 @@
+package ua.rd.pizza_service;
+
+import java.util.Arrays;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ua.rd.pizza_service.domain.Order;
+import ua.rd.pizza_service.services.OrderService;
+
+public class SpringAppRunner {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext repoContext = 
+				new ClassPathXmlApplicationContext("repoContext.xml");
+		
+		System.out.println(Arrays.toString(repoContext.getBeanDefinitionNames()));
+		
+		
+		ConfigurableApplicationContext appContext = 
+				new ClassPathXmlApplicationContext(new String[]{"appContext.xml"}, repoContext);
+		
+		
+		System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
+		
+
+		OrderService orderService = (OrderService)appContext.getBean("orderService");
+		Order order = orderService.placeNewOrder(null,  2 , 3);
+		
+		System.out.println(order);
+		
+		
+		repoContext.close();
+		appContext.close();
+	}
+}
